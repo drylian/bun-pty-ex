b8fb251efc25c2ed7725bc41678c1a6d9e0c5e4b
Drylian <109999325+drylian@users.noreply.github.com>
2025-05-21 23:22:46 -0400

    att

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 import { spawn } from "bun-pty";
 import path from "path";
 
-const pty = spawn("TerrariaServer.bin.x86_64", [], {
+// Game for all platform
+const linux_app = ["Linux", "TerrariaServer.bin.x86_64"];
+const windows_app = ["Windows", "TerrariaServer.exe"];
+const mac_app = ["Mac/Terraria Server.app/Contents/MacOS", "TerrariaServer"];
+
+const plt = process.platform;
+// select current platform
+const executable = plt == "win32"
+  ? windows_app
+  : plt == "darwin"
+    ? mac_app
+    : linux_app;
+
+// execute
+const pty = spawn(executable[1]!, [], {
   name: "xterm-256color",
-  cwd: path.join(import.meta.dirname, "./Linux"),
+  cwd: path.join(import.meta.dirname, "System", executable[0]!,),
   cols: 80,
   rows: 24
 })
@@ -14,5 +28,6 @@ pty.onExit(a => console.log(a));
  * await raised to ensure that the console
  * will be at the perfect moment of interaction 
  */
-await Bun.sleep(8000) 
-pty.write("\r\nn");
+await Bun.sleep(8000)
+// new world action 
+pty.write("n");
